982eacd0a714675ff2ca2cfa29a81021d1a9e9ec
Cosmin Iancu <ianstecos@gmail.com>
2024-08-15 17:35:06 -0500

    chore: update npm dependencies and add rollup-plugin-postcss for CSS bundling

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,10 +1,11 @@
-import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
+import json from '@rollup/plugin-json';
+import resolve from '@rollup/plugin-node-resolve';
 import typescript from '@rollup/plugin-typescript';
 import dts from 'rollup-plugin-dts';
-import json from '@rollup/plugin-json';
-
+import postcss from "rollup-plugin-postcss";
 import packageJson from './package.json' with { type: 'json' };
+
 
 export default [
   {
@@ -25,11 +26,13 @@ export default [
       resolve(),
       commonjs(),
       typescript({ tsconfig: './tsconfig.json' }),
+      postcss()
     ],
   },
   {
     input: 'dist/esm/types/index.d.ts',
     output: [{ file: 'dist/index.d.ts', format: 'esm' }],
     plugins: [json(), dts()],
+    external: [/\.css$/],
   },
 ];
