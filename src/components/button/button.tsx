4ce85939969013a39075bcd7dfe41b751b5db673
Cosmin Iancu <ianstecos@gmail.com>
2024-08-15 15:13:30 -0500

    chore: add Button component to the project

---
+++ src/components/button/button.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+interface ButtonProps {
+  children: React.ReactNode;
+  className: string;
+  type?: 'button' | 'submit' | 'reset';
+}
+
+const Button = (props: ButtonProps) => {
+  const { children, className, type = 'button' } = props;
+
+  return (
+    <button className={className} type={type}>
+      {children}
+    </button>
+  );
+};
+
+export default Button;
