06d52bb0da4e2e9c838d03ca12d8e9e82b4134d5
Cosmin Iancu <ianstecos@gmail.com>
2024-08-16 22:28:15 -0500

    chore: implement Prettier and format src

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -1,19 +1,19 @@
-import React from 'react';
+import React from "react";
 
 interface ButtonProps {
-  children: React.ReactNode;
-  className: string;
-  type?: 'button' | 'submit' | 'reset';
+    children: React.ReactNode;
+    className: string;
+    type?: "button" | "submit" | "reset";
 }
 
 const Button = (props: ButtonProps) => {
-  const { children, className, type = 'button' } = props;
+    const { children, className, type = "button" } = props;
 
-  return (
-    <button className={className} type={type}>
-      {children}
-    </button>
-  );
+    return (
+        <button className={className} type={type}>
+            {children}
+        </button>
+    );
 };
 
 export default Button;
