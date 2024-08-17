f0bd06efa53d23d3ee945000afdfb05184ca278c
Cosmin Iancu <ianstecos@gmail.com>
2024-08-16 22:46:47 -0500

    chore: integrate Prettier with ESLint and update tsconfig

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,19 +1,21 @@
-import pluginJs from '@eslint/js';
-import pluginReact from 'eslint-plugin-react';
-import globals from 'globals';
-import tseslint from 'typescript-eslint';
+import pluginJs from "@eslint/js";
+import eslintConfigPrettier from "eslint-config-prettier";
+import pluginReact from "eslint-plugin-react";
+import globals from "globals";
+import tseslint from "typescript-eslint";
 
 export default [
-  { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
-  {
-    settings: {
-      react: {
-        version: 'detect',
-      },
+    { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
+    {
+        settings: {
+            react: {
+                version: "detect",
+            },
+        },
+        languageOptions: { globals: { ...globals.browser, ...globals.node } },
     },
-    languageOptions: { globals: { ...globals.browser, ...globals.node } },
-  },
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
-  pluginReact.configs.flat.recommended,
+    pluginJs.configs.recommended,
+    ...tseslint.configs.recommended,
+    pluginReact.configs.flat.recommended,
+    eslintConfigPrettier,
 ];
