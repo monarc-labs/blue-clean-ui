9e13f41b52e9150d86877c9d1a43f0d663197b6e
Cosmin Iancu <ianstecos@gmail.com>
2024-08-16 22:07:37 -0500

    chore: add ESLint for code linting

---
+++ eslint.config.mjs
@@ -0,0 +1,19 @@
+import pluginJs from '@eslint/js';
+import pluginReact from 'eslint-plugin-react';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+
+export default [
+  { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
+  {
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+    languageOptions: { globals: { ...globals.browser, ...globals.node } },
+  },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+];
