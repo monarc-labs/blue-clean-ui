3ffa720ba262bfcbca29c8ffc4a1947c96c1c0c6
Cosmin Iancu <ianstecos@gmail.com>
2024-08-25 18:01:22 -0500

    revert: remove jest dependency and related files

--- src/components/button/button.test.tsx
+++
@@ -1,9 +0,0 @@
-import { render } from "@testing-library/react";
-import React from "react";
-import Button from "./button";
-
-describe("Button", () => {
-    test("renders the Button component", () => {
-        render(<Button>Button Label</Button>);
-    });
-});
